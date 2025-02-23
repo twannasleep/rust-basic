63419997b8303da3b0308b88849686b2b90d46ce
Tuanpep <tuanbt209@gmail.com>
2025-02-23 13:59:37 +0700

    hello_rust

---
+++ 00_introduction/exercises/hello_rust/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let myname = "Twan";
+    let target = "🦀 Rustaceans!";       
+    let greeting = "Great";
+    println!("Hello, I'm {} and i will become a {}", myname, target);
+    println!("Welcome to Rust programing language 🔥");
+    println!("{}, Your first Rust program is runing successful! ✅", greeting);
+}
